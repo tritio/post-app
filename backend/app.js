0bc34d7d8b3d68e9108247dc830bcaa930b3f4f5
Gonzalo G <coalescencia1@gmail.com>
2020-04-23 19:31:43 +0200

    create crud post and imageupload logic

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const usuario = require('./datos');
+
+const postsRoutes = require('./routes/posts');
+
+const app = express();
+
+mongoose.connect(
+  `mongodb+srv://${usuario.USER}:${usuario.PASS}@cluster0-dbncx.mongodb.net/node-angular?retryWrites=true&w=majority`,
+  { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('Conectado a la base de datos');
+    console.log(usuario.USER)
+  })
+  .catch(() => {
+    console.log('Conexión FALLIDA a la base de datos');
+  });
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
+})
+
+app.use('/api/posts', postsRoutes);
+
+module.exports = app;
---
+++ backend/routes/posts.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const Post = require('../models/post');
+const multer = require('multer'); // permite extraer ficheros enviados al servidor
+
+const router = express.Router();
+
+const MIME_TYPE_MAP =  {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mime type');
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images")   // primer parámetro es el error, segundo parámetro la carpeta en la dirección relativa a donde esté server.js
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+})
+
+router.post('',multer({storage: storage}).single("image"), (req, res, next) => {
+  const url = req.protocol + "://" + req.get("host");
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: url + "/images/" + req.file.filename  // req.file nos lo proporciona multer
+  })
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post agregado correctamente',
+      post: {
+        ...createdPost,
+        id: createdPost._id
+      }
+  })
+  });
+});
+
+router.put('/:id',multer({storage: storage}).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + "://" + req.get("host");
+    imagePath = url + "/images/" + req.file.filename;
+  }
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: imagePath
+
+  });
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    res.status(200).json({message: "actualizado correctamente"});
+  });
+});
+
+router.get('',(req, res, next) => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  let fetchedPosts;
+  if (pageSize && currentPage) {  // si alguno de estos valores es cero no pasa por el if
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery
+    .then((documents) => {
+      fetchedPosts = documents;
+      return Post.count();  // si hacemos un return en un then block, estamos creando otra promesa
+    })
+    .then(count => {
+      res.status(200).json({
+          message: 'Post obtenido correctamente',
+          posts: fetchedPosts,
+          maxPosts: count
+        });
+    } )
+});
+
+router.get("/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({message: 'Post no encontrado'});
+    }
+  })
+});
+
+router.delete('/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Post borrado'});
+  })
+});
+
+module.exports = router;
